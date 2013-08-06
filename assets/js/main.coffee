b12c0f1bffa9a937c65c186e9c68adc75e89167e
Sean Lang <slang800@gmail.com>
2013-08-06 09:10:58 -0500

    implemented radio-button based nav
    Also used a Jade "for" loop to DRY up the markup

--- assets/js/main.coffee
+++ assets/js/main.coffee
@@ -5,6 +5,7 @@ require.config(
 		underscore: '../components/underscore/underscore'
 		backbone: '../components/backbone/backbone'
 		jquery: '../components/jquery/jquery.min'
+		JSONP: '../js/jsonp'
 	shim:
 		underscore:
 			exports: '_'
@@ -14,9 +15,19 @@ require.config(
 )
 
 
-require ['jquery'], ($) ->
-	$.ajax(
-		url: "#{BACKEND_URL}/?json=1"
-	).done( (data) ->
+require ['jquery', 'JSONP'], ($, JSONP) ->
+	jsonp = new JSONP()
+
+	jsonp.get("#{BACKEND_URL}/?json=1", {}, (data) ->
 		console.log data
+		for post in data['posts']
+			$('body').append(
+				"""
+				<section>
+					<h1 class="title">#{post['title']}</h1>
+					<span class="author">by #{post['author']['name']}</span>
+					#{post['content']}
+				<section>
+				"""
+			)
 	)
--- public/js/main.js
+++ public/js/main.js
@@ -7,7 +7,8 @@
     paths: {
       underscore: '../components/underscore/underscore',
       backbone: '../components/backbone/backbone',
-      jquery: '../components/jquery/jquery.min'
+      jquery: '../components/jquery/jquery.min',
+      JSONP: '../js/jsonp'
     },
     shim: {
       underscore: {
@@ -20,11 +21,19 @@
     }
   });
 
-  require(['jquery'], function($) {
-    return $.ajax({
-      url: "" + BACKEND_URL + "/?json=1"
-    }).done(function(data) {
-      return console.log(data);
+  require(['jquery', 'JSONP'], function($, JSONP) {
+    var jsonp;
+    jsonp = new JSONP();
+    return jsonp.get("" + BACKEND_URL + "/?json=1", {}, function(data) {
+      var post, _i, _len, _ref, _results;
+      console.log(data);
+      _ref = data['posts'];
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        post = _ref[_i];
+        _results.push($('body').append("<section>\n	<h1 class=\"title\">" + post['title'] + "</h1>\n	<span class=\"author\">by " + post['author']['name'] + "</span>\n	" + post['content'] + "\n<section>"));
+      }
+      return _results;
     });
   });
 
